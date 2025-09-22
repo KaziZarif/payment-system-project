e2c845cda7ef36411d93c79460d73ac3c1d5d944
Zarif Atique <zarifatique02@gmail.com>
2025-09-22 23:19:02 +1000

    Handle case when payment is sucessful or cancelled

--- routes/payments.py
+++ routes/payments.py
@@ -1,6 +1,6 @@
 import os 
 import stripe 
-from flask import Blueprint, redirect
+from flask import Blueprint, redirect, render_template
 
 payments_bp = Blueprint("payments", __name__)
 
@@ -14,15 +14,23 @@ def create_checkout_session():
             line_items=[{
                 "price_data": {
                     "currency": "aud",
-                    "product_data": {"name": "Cup"},
+                    "product_data": {"name": "Stanley Cup"},
                     "unit_amount": 1500, 
                 }, 
                 "quantity": 1
             }],
             mode="payment",
-            success_url="http://localhost:5000/success",
-            cancel_url="http://localhost:5000/cancel",
+            success_url="http://localhost:5000/payments/success",
+            cancel_url="http://localhost:5000/payments/cancel",
         )
         return redirect(session.url, code=303)
     except Exception as e:
         return {"error": str(e)}, 400
+    
+@payments_bp.route("/success")
+def success():
+    return "<h1>Payment successful ✅</h1><p>Thank you for your order!</p>"
+
+@payments_bp.route("/cancel")
+def cancel():
+    return "<h1>Payment canceled ❌</h1><p>You can try again whenever you’re ready.</p>"
