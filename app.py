be9e48b70eb5c2ee9f474771607d451d884d24b9
Zarif Atique <zarifatique02@gmail.com>
2025-09-22 16:57:14 +1000

    Register routes

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from routes.payments import payments_bp
+from routes.shipping import shipping_bp
+
+def create_ap():
+    app = Flask(__name__)
+
+
+    app.register_blueprint(payments_bp, url_prefix="/payments")
+    app.register_blueprint(shipping_bp, url_prefix="/shipping")
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(port=5000, debug=True)
